f42031162856053914a84ca9fcd79dd93619c440
leonardopramos <leonardopreczevskiramos@gmail.com>
2023-08-24 13:47:39 -0300

    Added 1 mock test

--- src/test/java/br/com/portifolioleonardo/restwithspringbootandjava/unittests/mockito/services/PersonServicesTest.java
+++ src/test/java/br/com/portifolioleonardo/restwithspringbootandjava/unittests/mockito/services/PersonServicesTest.java
@@ -1,18 +1,44 @@
 package br.com.portifolioleonardo.restwithspringbootandjava.unittests.mockito.services;
 
+import br.com.portifolioleonardo.restwithspringbootandjava.model.Person;
+import br.com.portifolioleonardo.restwithspringbootandjava.repositories.PersonRepository;
+import br.com.portifolioleonardo.restwithspringbootandjava.services.PersonServices;
+import br.com.portifolioleonardo.restwithspringbootandjava.unittests.mapper.mocks.MockPerson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 class PersonServicesTest {
 
+    MockPerson input;
+
+    @InjectMocks
+    private PersonServices service;
+    @Mock
+    PersonRepository personRepository;
+
     @BeforeEach
-    void setUp() {
+    void setUpMocks() {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     void findAll() {
+
     }
 
     @Test
@@ -28,7 +54,21 @@ class PersonServicesTest {
     }
 
     @Test
-    void findById() {
+    void findById() throws Exception {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("[</person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+
     }
 
     @Test
